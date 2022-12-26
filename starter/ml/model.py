b85720530b491414555ac558832b13cd5ee3c2f9
Kwang Ho Hur <hurxx018@gmail.com>
2022-12-26 10:44:49 -0800

    feat: update steps of training a ml model
    Use random forest model with CV.
    Update type hints on functions in model.py

---
+++ starter/ml/model.py
@@ -0,0 +1,88 @@
+from typing import Tuple
+
+import numpy as np
+
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+
+
+# Optional: implement hyperparameter tuning.
+def train_model(
+        X_train: np.ndarray,
+        y_train: np.ndarray
+    ) -> RandomForestClassifier:
+    """
+    Trains a machine learning model and returns it.
+
+    Inputs
+    ------
+    X_train : np.array
+        Training data.
+    y_train : np.array
+        Labels.
+
+    Returns
+    -------
+    model
+        Trained machine learning model.
+    """
+
+    # model with grid search
+    rfc = RandomForestClassifier(random_state=42)
+
+    param_grid = {
+        'n_estimators': [200, 500],
+        'max_features': ['auto', 'sqrt'],
+        'max_depth': [4, 5, 100],
+        'criterion': ['gini', 'entropy']
+    }
+
+    cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+    cv_rfc.fit(X_train, y_train)
+
+    return cv_rfc.best_estimator_
+
+
+def compute_model_metrics(
+        y: np.ndarray,
+        preds: np.ndarray
+    ) -> Tuple[float, float, float]:
+    """
+    Validates the trained machine learning model using precision, recall, and F1.
+
+    Inputs
+    ------
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+    """
+    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
+    precision = precision_score(y, preds, zero_division=1)
+    recall = recall_score(y, preds, zero_division=1)
+    return precision, recall, fbeta
+
+
+def inference(model, X: np.ndarray) -> np.ndarray:
+    """ Run model inferences and return the predictions.
+
+    Inputs
+    ------
+    model : ???
+        Trained machine learning model.
+    X : np.array
+        Data used for prediction.
+    Returns
+    -------
+    preds : np.array
+        Predictions from the model.
+    """
+    return model.predict(X)
