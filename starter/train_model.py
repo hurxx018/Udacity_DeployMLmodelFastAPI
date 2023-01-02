d4bdcd2e72bb4f10ca298fae038302af13472332
Kwang Ho Hur <hurxx018@gmail.com>
2023-01-02 15:04:08 -0800

    update train_model.py

--- starter/train_model.py
+++ starter/train_model.py
@@ -23,56 +23,60 @@ PATH_TO_MODEL = os.path.join(os.path.dirname(__file__), "..", "model")
 PATH_TO_DATA = os.path.join(os.path.dirname(__file__), "..", "data")
 
 
-# Add code to load in the data.
-data = pd.read_csv(os.path.join(PATH_TO_DATA, "clean_data.csv"), sep=",")
+def main():
+    # Add code to load in the data.
+    data = pd.read_csv(os.path.join(PATH_TO_DATA, "clean_data.csv"), sep=",")
 
-label_name = "salary"
+    label_name = "salary"
 
 
-# train-test split.
-train, test = train_test_split(data, test_size=0.20, random_state=42)
+    # train-test split.
+    train, test = train_test_split(data, test_size=0.20, random_state=42)
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label=label_name, training=True
-)
-
-# Proces the test data with the process_data function.
-X_test, y_test, _, _ = process_data(
-    test, categorical_features=cat_features, label=label_name,
-    training=False, encoder=encoder, lb=lb
-)
-
-# Train and save a model.
-model = train_model(X_train, y_train)
-
-save_model(PATH_TO_MODEL, model, encoder, lb)
-
-# Load model and check its performance
-model, encoder, lb = load_model(PATH_TO_MODEL)
-
-y_test_preds = inference(model, X_test)
-
-precision, recall, fbeta = compute_model_metrics(y_test, y_test_preds)
-print(f"Evaluation on Test: \
-    Precision {precision}, Recall {recall}, Fbeta {fbeta}")
-
-with open("slice_output.txt", "w") as ftxt:
-    result_slicing = performance_slice_on_feature(
-        model, train, cat_features[0], cat_features, label_name, encoder, lb
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label=label_name, training=True
     )
-    ftxt.write(
-        "".join(
-            [f"{k}: Precision {v[0]}, Recall {v[1]}, Fbeta {v[2]}\n"
-                for k, v in result_slicing.items()]
+
+    # Proces the test data with the process_data function.
+    X_test, y_test, _, _ = process_data(
+        test, categorical_features=cat_features, label=label_name,
+        training=False, encoder=encoder, lb=lb
+    )
+
+    # Train and save a model.
+    model = train_model(X_train, y_train)
+
+    save_model(PATH_TO_MODEL, model, encoder, lb)
+
+    # Load model and check its performance
+    model, encoder, lb = load_model(PATH_TO_MODEL)
+
+    y_test_preds = inference(model, X_test)
+
+    precision, recall, fbeta = compute_model_metrics(y_test, y_test_preds)
+    print(f"Evaluation on Test: \
+        Precision {precision}, Recall {recall}, Fbeta {fbeta}")
+
+    with open("slice_output.txt", "w") as ftxt:
+        result_slicing = performance_slice_on_feature(
+            model, test, cat_features[0], cat_features, label_name, encoder, lb
         )
-    )
+        ftxt.write(
+            "".join(
+                [f"{k}: Precision {v[0]}, Recall {v[1]}, Fbeta {v[2]}\n"
+                    for k, v in result_slicing.items()]
+            )
+        )
+
+if __name__ == "__main__":
+    main()
